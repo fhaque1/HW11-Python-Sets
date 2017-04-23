7e1dd4996c711e32c1d36fc5e0c65a086381d8ee
Edmond-Lam <elam3@stuy.edu>
2017-04-23 15:07:11 -0400

    finished

--- set.py
+++ set.py
@@ -6,7 +6,16 @@ def intersection(a,b):
 	
 def set_difference(u,a):
 	return sorted([x for x in u if x not in a])
-	
+
+def simmer_differ(a,b):
+        return sorted([x for x in a if x not in b] + [x for x in b if x not in a])
+
+def cartesian_product(a,b):
+        return sorted([[x,y] for x in a for y in b])
+
 print union([1,2,3],[2,3,4])
 print intersection([1,2,3],[2,3,4])
 print set_difference([1,2,3],[2,3,4])
+print simmer_differ([1,2,3],[2,3,4])
+
+print cartesian_product([1,2],["red","white"])
