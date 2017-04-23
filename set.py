7f531df655c08aee97b50e5d8dccda0bfee81745
fhaque1 <farhan3231@gmail.com>
2017-04-22 22:16:53 -0400

    union, intersect, and set diff done

--- set.py
+++ set.py
@@ -2,9 +2,11 @@ def union(a,b):
 	return sorted([x for x in b if x not in a] + a)
 
 def intersection(a,b):
-	pass
+	return sorted([x for x in b if x in a])
 	
-def set_difference(a,b):
-	pass
-
-print union([1,2,4,6],[1,2,3,4,5])
+def set_difference(u,a):
+	return sorted([x for x in u if x not in a])
+	
+print union([1,2,3],[2,3,4])
+print intersection([1,2,3],[2,3,4])
+print set_difference([1,2,3],[2,3,4])
